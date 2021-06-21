90cf4e63ea1559b291aac27f9d614c332fe06a61
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-21 13:12:08 +0530

    Added slug generating middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -97,6 +98,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create Bootcamp slug from name
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Bootcamp = mongoose.model('Bootcamp', BootcampSchema);
