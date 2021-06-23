3a48cb288a6393f52f6ee058293f2dffacbeb408
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-23 10:22:28 +0530

    Logout user and clear token cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,21 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Log out user and clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 5 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @desc    Get current logged in User
 // @route   POST /api/v1/auth/me
 // @access  Private
--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,13 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
+  logout,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.post('/forgotPassword', forgotPassword);
 router.put('/resetPassword/:resetToken', resetPassword);
