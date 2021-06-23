aa444544215312549f040f1944a4fe8dc622ae95
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-23 09:36:08 +0530

    Average rating for bootcamp calculated

--- models/Review.js
+++ models/Review.js
@@ -34,6 +34,39 @@ const ReviewSchema = new mongoose.Schema({
   },
 });
 
+// Static method to get average of rating
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// Call getAverageRating after 'save'
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before 'remove'
+ReviewSchema.pre('remove', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 // One user => one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
